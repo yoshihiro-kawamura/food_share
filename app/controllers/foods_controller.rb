d4718b14078dae0ace2b111a4b28f289e50433ed
yoshihiro-kawamura <73100863+yoshihiro-kawamura@users.noreply.github.com>
2020-12-02 00:37:52 +0900

    コントローラー（ストロングパラメーター/create・newアクション）

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,27 @@
 class FoodsController < ApplicationController
   def index
+    @food = Food.all
+  end
+
+  def new
+    @food = Food.new()
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :example, :category_id ,:price, :image).merge(user_id: current_user.id)
   end
 end
+
+
+# :category_id,
