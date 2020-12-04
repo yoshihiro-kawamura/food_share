8e84643ddd1faf1f4480a738b844ed75ebc6221b
yoshihiro-kawamura <73100863+yoshihiro-kawamura@users.noreply.github.com>
2020-12-05 01:11:26 +0900

    modelに追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,25 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  # deviseでomniauthを使うための記述
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+  
+  has_many :sns_credentials, dependent: :destroy
   has_many :foods
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    # 2回目以降はこのUserが使われる↓
+    { user: user, sns: sns }
+  end
 end
