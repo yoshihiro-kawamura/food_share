2b95c49b026233a0b5019d72eb8ff184d09a2225
yoshihiro-kawamura <73100863+yoshihiro-kawamura@users.noreply.github.com>
2020-12-05 01:09:20 +0900

    Routingの作成（deviseとは別）

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  # deviseが提供するactionの内容を再設定する
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
   }
+    resources :users, only: :new
+    
   root to: "foods#index"
   resources :foods do
     resources :comments, only: [:show, :create]
   end
-  resources :users, only: :new
 end
